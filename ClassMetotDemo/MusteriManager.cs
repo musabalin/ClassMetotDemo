6bd4616e136a6c76c4e14970f52fc3c747916b3e
MUSA BALIN <20164753049@ogr.akdeniz.edu.tr>
2021-09-21 23:11:14 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    public class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad+" Eklendi");
+        }
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad+" Silindi.");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Musteri m1 = new Musteri();
+            m1.Ad = "Musa";
+            m1.Soyad = "Balın";
+            m1.Id = 1;
+            Musteri m2 = new Musteri();
+            m2.Id = 2;
+            m2.Ad = "Burak";
+            m2.Soyad = "Soycu";
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(m1);
+            musteriManager.Sil(m2);
+        }
+    }
+}
